dfbd6d277ab9aeb5faa55cab4db9c2a75aa8bfb6
UgoPelissier <upelissier@gmail.com>
2023-07-29 15:55:21 +0000

    🧑‍💻 Add env variable for test script

--- scripts/meshnet/test.sh
+++ scripts/meshnet/test.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
-ckpt_path=/data/users/upelissier/30-Code/meshnet/logs/version_7/checkpoints/epoch=99-step=1000.ckpt
+ckpt_path=/home/eleve05/adaptnet/meshnet/logs/version_1/checkpoints/epoch=99-step=2400.ckpt
 
 clear
+
+for arg in "$@"
+do
+   key=$(echo $arg | cut -f1 -d=)
+
+   key_length=${#key}
+   value="${arg:$key_length+1}"
+
+   export "$key"="$value"
+done
+
 cd $PYTHONPATH
-python meshnet/main.py test --ckpt_path $ckpt_path
+python meshnet/main.py test -c meshnet/configs/${env}.yaml --ckpt_path $ckpt_path
