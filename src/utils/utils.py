46332034813bdb10a0856bc45fc9180bb51360a8
UgoPelissier <upelissier@gmail.com>
2023-09-05 07:27:17 +0000

    ⚡️ Update traingle to edges function for 3d dataset

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -15,23 +15,33 @@ def node_type(label: str) -> int:
         else:
             return NodeType.NORMAL
 
-
-def triangles_to_edges(faces: torch.Tensor) -> torch.Tensor:
-        """Computes mesh edges from triangles."""
-        # collect edges from triangles
+def triangles_to_edges(dim: int, faces: torch.Tensor) -> torch.Tensor:
+    """Computes mesh edges from triangles."""
+    # collect edges from triangles
+    if (dim == 2):
         edges = torch.vstack((faces[:, 0:2],
-                              faces[:, 1:3],
-                              torch.hstack((faces[:, 2].unsqueeze(dim=-1),
+                            faces[:, 1:3],
+                            torch.hstack((faces[:, 2].unsqueeze(dim=-1),
                                             faces[:, 0].unsqueeze(dim=-1)))
                             ))
-        receivers = torch.min(edges, dim=1).values
-        senders = torch.max(edges, dim=1).values
-        packed_edges = torch.stack([senders, receivers], dim=1)
-        # remove duplicates and unpack
-        unique_edges = torch.unique(packed_edges, dim=0)
-        senders, receivers = unique_edges[:, 0], unique_edges[:, 1]
-        # create two-way connectivity
-        return torch.stack([torch.cat((senders, receivers), dim=0), torch.cat((receivers, senders), dim=0)], dim=0)
+    elif (dim == 3):
+        edges = torch.vstack((faces[:, 0:2],
+                            faces[:, 1:3],
+                            faces[:, 2:4],
+                            torch.hstack((faces[:, 3].unsqueeze(dim=-1),
+                                            faces[:, 0].unsqueeze(dim=-1)))
+                            ))
+    else:
+        raise ValueError("The dimension must be either 2 or 3.")
+    receivers = torch.min(edges, dim=1).values
+    senders = torch.max(edges, dim=1).values
+    packed_edges = torch.stack([senders, receivers], dim=1)
+    # remove duplicates and unpack
+    unique_edges = torch.unique(packed_edges, dim=0)
+    senders, receivers = unique_edges[:, 0], unique_edges[:, 1]
+    # create two-way connectivity
+    return torch.stack([torch.cat((senders, receivers), dim=0), torch.cat((receivers, senders), dim=0)], dim=0)
+
 
 
 def write_field(path:str, field: torch.Tensor, name: str) -> None:
